29ce4724b4ec2fc7c7566fcf89d0590aba7d1f48
vasistasaimagam6 <115581017+vasistasaimagam6@users.noreply.github.com>
2024-06-24 16:02:58 +0530

    add

--- frontend/vite-project/src/pages/Signup.tsx
+++ frontend/vite-project/src/pages/Signup.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { Auth } from "../components/Auth";
 import { Quote } from "../components/Quote";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 
 export default function Signup(){
     const navigate= useNavigate()
-    const [isLoading,setIsLoading]=useState(false)
    
     useEffect(()=>{
         if(localStorage.getItem("token")){
